5e4a9811d2de51302ea508b56d37d77d7f98426f
Trenton <lotrentonlawrence@gmail.com>
2026-07-04 07:27:51 -0500

    Set real Firebase web config values

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 export const FIREBASE_CONFIG = {
-  apiKey: "REPLACE_WITH_FIREBASE_API_KEY",
-  authDomain: "REPLACE_WITH_FIREBASE_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_FIREBASE_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_FIREBASE_APP_ID"
+  apiKey: "AIzaSyBoaorOhHd2g5VkfyxHDaMS1e-uwjZH6Ac",
+  authDomain: "sweet-home-transitions.firebaseapp.com",
+  projectId: "sweet-home-transitions",
+  storageBucket: "sweet-home-transitions.firebasestorage.app",
+  messagingSenderId: "403499275555",
+  appId: "1:403499275555:web:7ad222213584f591642896"
 };
